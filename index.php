a32b49cbc17cc56b676a915125d472fcec9e7d9e
juanmuno318 <juanmuno@student.arteveldehs.be>
2017-10-05 09:43:19 +0200

    oefeningen 3

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="container">
+    <?php 
+
+
+    $x= 10;
+
+    $y= 7;
+
+    echo '<p>'.($x+$y).'</p>';
+    echo '<p>'.($x-$y).'</p>';
+    echo '<p>'.($x*$y).'</p>';
+    echo '<p>'.($x/$y).'</p>';
+    echo '<p>'.($x%$y).'</p>';
+
+    ?>
+    
+    </div>
+</body>
+</html>
